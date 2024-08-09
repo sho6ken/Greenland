1b7b46da31f76a58ec31a1c12a544ed1a2f7f6a6
Kent <>
2024-08-09 15:43:42 +0800

    style: split single base & single mgr

--- single/single_mgr.ts
+++ single/single_mgr.ts
@@ -1,57 +1,5 @@
 import { Logger } from "../log/logger";
-
-/**
- * 單例介面
- */
-export interface Singleton {
-    /**
-     * 名稱
-     */
-    name: string;
-
-    /**
-     * 常駐不釋放
-     */
-    hold?: boolean;
-
-    /**
-     * 初始化
-     */
-    init(...params: any[]): any;
-
-    /**
-     * 釋放
-     */
-    free(...params: any[]): any;
-
-    /**
-     * 清理
-     * @param params 
-     */
-    clear(...params: any[]): any;
-}
-
-/**
- * 單例類別
- * @summary 限制單例類別必須具備以下功能
- */
-interface SingleClass<T extends Singleton> {
-    /**
-     * 功能
-     */
-    name: string;
-
-    /**
-     * 實例
-     * @summary 當實例已在其他地方建立, 則使用此實例並跳過創建, 通常用在cc.component的單例
-     */
-    inst?: T;
-
-    /**
-     * 建構子
-     */
-    new(): T;
-}
+import { SingleClass, Singleton } from "./singleton";
 
 /**
  * 單例管理
